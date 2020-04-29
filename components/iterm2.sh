cd0fca62280529a99ced6b60f670a165a67b977e
Gigi <the.gigi@gmail.com>
2020-04-29 02:57:29 -0700

    added AWS Profile user var to be used in the status bar

--- components/iterm2.sh
+++ components/iterm2.sh
@@ -9,4 +9,7 @@ function set_iterm2_tab {
 function iterm2_print_user_vars() {
   # Create a variable `user.kube-context` that contains the current kube context
   iterm2_set_user_var kube-context "☸️  $(kubectl config current-context)"
+
+  # Create a variable `user.aws-profile` that contains the current AWS identity
+  iterm2_set_user_var aws-profile "AWS Profile: ${AWS_PROFILE}"
 }
